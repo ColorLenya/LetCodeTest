b35396397334f877d40d3d6c7e9681e40177f280
yinrufeng <yrf8888@qq.com>
2025-03-27 22:08:24 +0800

    main显示candy函数输出

--- LetCode3_19/src/main/java/org/example/Main.java
+++ LetCode3_19/src/main/java/org/example/Main.java
@@ -4,4 +4,5 @@ public class Main {
     public static void main(String[] args) {
        Solution a = new Solution();
        int[] ratings={1,3,2,2,1};
+        System.out.println(a.candy(ratings));
        a.candy(ratings);}}
