fff2cadd009c11bd4a71690a1ede0f2d788f56e0
ColourLenya <yrf8888@qq.com>
2025-03-28 14:50:14 +0800

    我删除了三个本地项目

--- LetCode3_19/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-       Solution a = new Solution();
-       int[] ratings={1,3,2,2,1};
-        System.out.println(a.candy(ratings));
-       a.candy(ratings);}}
